80b4f0ed7838ad03b7982f96a20ff1c4c41b2194
Rico-la <rico.alves91@gmail.com>
2022-02-16 11:17:43 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    flex: 1
+  },
+  containerA: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "grey",
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerB: {
+    backgroundColor: "lightgreen",
+    alignItems: 'flex-end',
+  },
+  blueText: {
+    color: 'blue'
+  }
 });
+
+
+export default function App() {
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.containerA}>
+      <Text style={styles.blueText}>Hello, world!</Text>
+      <Text style={styles.blueText}>Hello, world!</Text>
+      <Text style={styles.blueText}>Hello, world!</Text>
+      <Button title="This is a button" onPress={() => alert('prout') }/>
+      <StatusBar style="auto" />
+    </View>
+    <View style={styles.containerB}>
+      <Text style={styles.blueText}>Hello, world!</Text>
+      <Text style={styles.blueText}>Hello, world!</Text>
+      <Text style={styles.blueText}>Hello, world!</Text>
+    </View>
+    </View>
+  );
+}
